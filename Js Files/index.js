3dc2ef1fa62aaf10d8feb91aa1209a5c1d1e877c
Pratik Dole <pdoleofficial@gmail.com>
2023-08-21 16:31:56 +0530

    pop form error solved but Js not working

--- Js Files/index.js
+++ Js Files/index.js
@@ -1,18 +1,31 @@
-
 // We Can Create A Logic For The Pop-Up Form :: 
 
-// Creating A Function For Over-Writing The CSS Property :: 
-showUp = () => {
-    document.querySelector('.signUp').classList.add('.showSignUp');
+// Creating A Function For Over-Writing The CSS Property For SignUp Form :: 
+open1 = () =>{
+    console.log("I Am Click For Open")
+    const a = document.querySelector('.signUp');
+    a.classList.add('.showSignUp')
+    console.log(a)
 }
 
-closeUp = () => {
-    document.querySelector('.signUp').classList.remove('.showSignUp');
+close1 = () =>{
+    console.log("I Am Click For Close")
+    const b = document.querySelector('.signUp');
+    b.classList.remove('.showSignUp')
+    console.log(b)
 }
 
-let a = document.querySelector(".ncBton");
-a.addEventListener("click", showUp)
+// Creating A Function For Over-Writing The CSS Property For SignIn Form ::
+open2 = () =>{
+    console.log("I Am Click For Open")
+    const c = document.querySelector('.signIn');
+    c.classList.add('.showSignIn')
+    console.log(c)
+}
 
-let b = document.querySelector(".crossBton");
-b.addEventListener("click", closeUp)
-
+close2 = () =>{
+    console.log("I Am Click For Close")
+    const d = document.querySelector('.signIn');
+    d.classList.remove('.showSignIn')
+    console.log(d)
+}
